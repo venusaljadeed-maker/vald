8de8319adf1a0def31048cf53b72462b8f262415
venusaljadeed-maker <venusaljadeed@gmail.com>
2026-08-20 23:49:47 +0530

    style: remove metrics tracking section from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { Hero } from "@/components/hero/Hero";
 import { BrandStrip } from "@/components/brands/BrandStrip";
 import { ValdStandard } from "@/components/brand/ValdStandard";
 import { ProductEcosystem } from "@/components/products/ProductEcosystem";
-import { Metrics } from "@/components/home/Metrics";
 import { FinalCTA } from "@/components/quote/FinalCTA";
 
 export default function Home() {
@@ -13,7 +12,6 @@ export default function Home() {
       <BrandStrip />
       <ValdStandard />
       <ProductEcosystem />
-      <Metrics />
       <FinalCTA />
     </>
   );
